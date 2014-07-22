3f7a33d0603fe0ddbd52c7ea1c3d2198c15201e7
jtrussell <jus.russell@gmail.com>
2014-07-21 20:30:12 -0400

    test: Add tests for jade engine and custom opts

--- test/bedecked-test.js
+++ test/bedecked-test.js
@@ -1,11 +1,14 @@
 /*global describe, it, before, beforeEach */
 /*jshint -W030 */
+/*jshint -W054 */
 
 'use strict';
 
 var join = require('path').join
   , fs = require('fs')
+  , _ = require('lodash')
   , expect = require('chai').expect
+  , sinon = require('sinon')
   , cheerio = require('cheerio')
   , bd = require('../lib/bedecked');
 
@@ -35,6 +38,10 @@ describe('bedecked', function() {
       expect($('.slides > section').eq(2).find('section').length).to.equal(2);
     });
 
+    it('should not leave any mustache templates', function() {
+      expect($.html()).to.not.match(/{{[^}]+}}/g);
+    });
+
     it('should add styles and scripts to the page', function() {
       expect($('link#reveal-core').length).to.equal(1);
       expect($('link#reveal-theme').length).to.equal(1);
@@ -42,15 +49,72 @@ describe('bedecked', function() {
       expect($('script#reveal-init').length).to.equal(1);
     });
 
-    it('should not leave any mustache templates', function() {
-      expect($.html()).to.not.match(/{{[^}]+}}/g);
+    it('should initialize revealjs', function() {
+      var guts = $('script#reveal-init').html();
+      var Reveal = {
+        initialize: sinon.spy()
+      };
+      var fn = new Function('Reveal', guts);
+      fn(Reveal);
+      expect(Reveal.initialize.calledOnce).to.be.true;
     });
   });
 
   describe('api custom opts', function() {
-    /**
-     * @todo
-     */
+    var err, $;
+
+    var bdOpts = {
+      engine: 'jade',
+      protocol: 'https:',
+      revealjsVersion: '3.7.6',
+      theme: 'night',
+      title: 'Awesome prez',
+    };
+
+    var revealOpts = {
+      optAutoSlide: 1,
+      optAutoSlideStoppable: false,
+      optCenter: false,
+      optControls: false,
+      optEmbedded: true,
+      optFragments: false,
+      optHideAddressBar: false,
+      optHistory: true,
+      optKeyboard: false,
+      optLoop: true,
+      optMouseWheel: true,
+      optOverview: false,
+      optPreviewLinks: true,
+      optProgress: false,
+      optRtl: true,
+      optSlideNumber: true,
+      optTouch: false,
+      optViewDistance: 7
+    };
+
+    before(function(done) {
+      bd(join(fxd, '002.jade'), _.extend({}, bdOpts, revealOpts), function(e, html) {
+        err = e;
+        $ = cheerio.load(html);
+        done();
+      });
+    });
+
+    it('should pass through options to reaveal.js', function() {
+      var guts = $('script#reveal-init').html();
+      var Reveal = {
+        initialize: sinon.spy()
+      };
+      var fn = new Function('Reveal', guts);
+      fn(Reveal);
+      var rOpts = {};
+      Object.keys(revealOpts).forEach(function(keyOrig) {
+        var key = keyOrig.slice(3);
+        key = key.charAt(0).toLowerCase() + key.slice(1);
+        rOpts[key] = revealOpts[keyOrig];
+      });
+      expect(Reveal.initialize.firstCall.args[0]).to.deep.equal(rOpts);
+    });
   });
 });
 
